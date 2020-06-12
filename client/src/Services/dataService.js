e48b9c21a949d89a9fa2b959678ac9f166a90d64
Seif Bouguila <seifbouguila@MacBook-Air-de-Seif.local>
2020-06-12 13:40:03 +0100

    test

--- client/src/Services/dataService.js
+++ client/src/Services/dataService.js
@@ -12,30 +12,13 @@ const serverBaseUrl = "https://cwc0sbvgf4.execute-api.eu-west-1.amazonaws.com/"
 
 
 export default {
-  getMovies: (category) => {
-    const url = `https://api.themoviedb.org/3/movie/${category}?api_key=${apiKey}&language=en-US&page=1`
-    return axios.get(url).then(info => info.data)
-  }, 
+  
   getMovies2: (category) => {
     const url = `https://cwc0sbvgf4.execute-api.eu-west-1.amazonaws.com/dev/items`
     return axios.get(url).then(info => info.data)
   },
-  getSearch: (query ) => {
-    const url = `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${apiKey}`
-    return axios.get(url).then(info => info.data)
-  },
-  getSearch2: (query ) => {
-    const url = `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${apiKey}`
-    return axios.get(url).then(info => info.data)
-  },
-  getMovieById: (movieId ) => {
-    const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&append_to_response=videos`
-    return axios.get(url).then(info => info.data)
-  },
-  getMostVoted: () => {
-    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=vote_average.asc&include_adult=true&include_video=false&page=1`
-    return axios.get(url).then(info => info.data)
-  },
+  
+ 
 
   addFilm: (title , description, datec , genre)=> {
     const url = serverBaseUrl + "dev/items "
